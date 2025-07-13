dceed5fb4e138a15e48a27db909b6802a572d358
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-07-13 23:24:05 +0300

    feat: implement main view component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,69 @@
 import { Component } from 'react';
+import CharacterCard from '../CharacterCard/CharacterCard';
 
-export default class Main extends Component {
+type MainProps = object;
+
+interface MainState {
+  characters: {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: { name: string; url: string };
+    location: { name: string; url: string };
+    image: string;
+    episode: string[];
+    url: string;
+    created: string;
+  }[];
+  loading: boolean;
+  error: null | string;
+}
+
+export default class Main extends Component<MainProps, MainState> {
+  constructor(props: MainProps) {
+    super(props);
+    this.state = {
+      characters: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  async componentDidMount(): void {
+    try {
+      const request = await fetch('https://rickandmortyapi.com/api/character');
+      if (!request.ok)
+        throw new Error('Could not load your favorite characters');
+      const parsed = await request.json();
+      this.setState({ characters: parsed.results, loading: false });
+    } catch (error: unknown) {
+      const message =
+        error instanceof Error ? error.message : 'Unknown error occurred';
+      this.setState({ error: message, loading: false });
+    }
+  }
+
   render() {
-    return <main>Main</main>;
+    const { characters, loading, error } = this.state;
+    if (loading) return <main>Loading...</main>;
+    if (error) return <main>Error: {error}</main>;
+    return (
+      <main className="bg-blue-900">
+        <div
+          className="flex flex-wrap gap-6 py-4 items-center justify-center"
+          aria-label="characters-cards-container"
+        >
+          {characters.map((characterInfo) => (
+            <CharacterCard
+              key={characterInfo.id}
+              characterInfo={characterInfo}
+            />
+          ))}
+        </div>
+      </main>
+    );
   }
 }
