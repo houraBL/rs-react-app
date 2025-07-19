3ff0eacea6242e32d58039bb6e31b389320441b5
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-07-20 01:18:38 +0800

    fix: check if href for links is present and check alt text for logos

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -6,7 +6,15 @@ import { render, screen } from '@testing-library/react';
 describe('Footer', () => {
   it('Renders Footer', () => {
     render(<Footer />);
-    expect(screen.getByText('RS School')).toBeInTheDocument();
-    expect(screen.getByText('hourabl')).toBeInTheDocument();
+    expect(screen.getByText('RS School')).toHaveAttribute(
+      'href',
+      'https://rs.school/'
+    );
+    expect(screen.getByText('hourabl')).toHaveAttribute(
+      'href',
+      'https://github.com/houraBL'
+    );
+    expect(screen.getByAltText('RS School logo')).toBeInTheDocument();
+    expect(screen.getByAltText('GitHub logo')).toBeInTheDocument();
   });
 });
