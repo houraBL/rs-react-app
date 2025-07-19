1aa877e73fdfdbe115b4f217eabf11c5013041ef
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-07-19 23:24:05 +0800

    feat: add Search Component Tests for Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,16 +1,56 @@
 import Header from './Header';
-import { describe, it } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-describe('testBlock', () => {
-  it('testName', () => {
-    render(
-      <Header
-        onSearchSubmit={(str) => {
-          console.log(str);
-        }}
-      />
-    );
+describe('Header', () => {
+  const submitFunction = vi.fn();
+  beforeEach(() => {
+    localStorage.clear();
+    vi.clearAllMocks();
+  });
+
+  describe('Search Component Tests', () => {
+    it('Renders search input and search button', () => {
+      render(<Header onSearchSubmit={submitFunction} />);
+      expect(screen.getByText('Look up a character:')).toBeInTheDocument();
+      expect(screen.getByText('Search')).toBeInTheDocument();
+      expect(screen.getByText('✕')).toBeInTheDocument();
+    });
+
+    it('Displays previously saved search term from localStorage on mount', () => {
+      localStorage.setItem('search', 'test');
+      render(<Header onSearchSubmit={submitFunction} />);
+      expect(screen.getByDisplayValue('test')).toBeInTheDocument();
+    });
+
+    it('Shows empty input when no saved term exists', () => {
+      render(<Header onSearchSubmit={submitFunction} />);
+      expect(screen.getByPlaceholderText('start typing...')).toHaveValue('');
+    });
+  });
+
+  describe('User Interaction Tests', () => {
+    it('Updates input value when user types', async () => {});
+
+    it('Saves search term to localStorage when search button is clicked', async () => {});
+
+    it('Trims whitespace from search input before saving', async () => {});
+
+    it('Clears search input and localstorage when clear search button is clicked', async () => {});
+
+    it('Triggers search callback with correct parameters', async () => {});
+  });
+
+  describe('LocalStorage Integration', () => {
+    it('Retrieves saved search term on component mount', () => {});
+
+    it('Overwrites existing localStorage value when new search is performed', () => {});
   });
 });
+
+/*
+  describe('testBlock', () => {
+    it('testName', () => {});
+  });
+*/
