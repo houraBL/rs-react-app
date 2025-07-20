cbf143cb4fbf581f834689f1121d2a1fe5dcd548
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-07-20 16:08:07 +0800

    fix: handle missing props

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,19 +1,30 @@
 import { Component } from 'react';
 import type { CharacterInfo } from '../../types/character';
 
-type CharacterCardProps = { characterInfo: CharacterInfo };
+type CharacterCardProps = { characterInfo?: CharacterInfo };
 
 export default class CharacterCard extends Component<CharacterCardProps> {
   render() {
     const { characterInfo } = this.props;
+    if (!characterInfo) {
+      return (
+        <div className="flex items-center justify-center bg-blue-500 rounded-3xl p-6 w-48 h-64">
+          <div className="text-lg text-white font-bold">
+            Character information is missing
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="flex flex-col gap-2 bg-blue-500 rounded-3xl p-6 w-48 h-64">
         <img
-          src={characterInfo.image}
+          src={characterInfo?.image}
           className="rounded-3xl"
           alt="Character portrait"
         />
-        <div className="text-lg text-white font-bold">{characterInfo.name}</div>
+        <div className="text-lg text-white font-bold">
+          {characterInfo?.name}
+        </div>
       </div>
     );
   }
