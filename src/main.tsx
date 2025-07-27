24457b5c33245d2fe1bcc4093b237054fe8a22a9
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-07-27 18:56:05 +0800

    chore: install lint-staged and react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+
+import { BrowserRouter, Route, Routes } from 'react-router';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
 );
