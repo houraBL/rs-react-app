a28123079fa9e060dd49065d3016503142859f24
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-07-28 03:51:30 +0800

    fix: update api call logic

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,22 +1,33 @@
-export async function fetchCharacters(searchTerm?: string) {
+export async function fetchCharacters(searchTerm?: string, page = 1) {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    const searchParams = searchTerm?.trim()
-      ? `/?name=${searchTerm.trim()}`
-      : '';
-    const request = await fetch(
-      `https://rickandmortyapi.com/api/character${searchParams}`
+    const params = new URLSearchParams();
+    if (searchTerm?.trim()) {
+      params.set('name', searchTerm.trim());
+    } else {
+      params.delete('name');
+    }
+    params.set('page', page.toString());
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?${params.toString()}`
     );
-    if (!request.ok) {
-      if (request.status === 404) return [];
+
+    if (!response.ok) {
+      if (response.status === 404) return { results: [], totalPages: 0 };
+      if (response.status === 500)
+        throw new Error('Could not load your favorite characters');
       throw new Error('Could not load your favorite characters');
     }
-    const parsed = await request.json();
+
+    const parsed = await response.json();
 
     if (!parsed || !Array.isArray(parsed.results)) {
       throw new Error('Cannot retrieve characters');
     }
-    return parsed.results;
+
+    return {
+      results: parsed.results,
+      totalPages: parsed.info.pages,
+    };
   } catch (error: unknown) {
     const message =
       error instanceof Error ? error.message : 'Unknown error occurred';
