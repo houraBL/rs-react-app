f8ac2a8a7246531a657a020bdf3079bc27b5c2e2
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-08-03 14:18:29 +0800

    style: fix styles for CharacterCard

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -31,18 +31,19 @@ export default function CharacterCard({
 
   return (
     <div
-      className="relative flex flex-col gap-2 bg-blue-500 rounded-3xl pt-4 px-2 sm:p-6 w-32 h-48 sm:w-48 sm:h-64 pb-0 sm:pb-0"
+      className="relative flex flex-col gap-2 bg-blue-500 rounded-3xl w-32 h-48 sm:w-48 sm:h-64"
       data-testid="character-card"
       role="character-card"
     >
       <input
+        role="selection-checkbox"
         type="checkbox"
         checked={!!selectedItems[characterInfo.id]}
         onChange={handleCheckboxChange}
         className="absolute top-0 right-0 sm:top-4 sm:right-4 w-4 h-4 hover:cursor-pointer border-blue-300 rounded-sm focus:ring-white focus:ring-2"
       />
       <Link to={`/${page}/${characterInfo.id}`}>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-full h-full pt-4 px-2 sm:p-6 pb-0 sm:pb-0">
           <img
             src={characterInfo?.image}
             className="rounded-3xl w-24 h-24 mx-auto sm:w-32 sm:h-32"
