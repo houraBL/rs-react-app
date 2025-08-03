3d87224eca402ebbc4b6c746b21d745623223363
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-08-03 14:05:16 +0800

    feat: add checkbox and state logic to CharacterCard

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,5 +1,8 @@
 import { Link, useParams } from 'react-router-dom';
 import type { CharacterInfo } from '../../types/character';
+import { toggleItem } from '../../store/selectionSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../../app/store';
 
 export default function CharacterCard({
   characterInfo,
@@ -7,6 +10,10 @@ export default function CharacterCard({
   characterInfo?: CharacterInfo;
 }) {
   const { pageId } = useParams();
+  const dispatch = useDispatch();
+  const selectedItems = useSelector(
+    (state: RootState) => state.selection.selectedItems
+  );
 
   if (!characterInfo) {
     return (
@@ -18,23 +25,34 @@ export default function CharacterCard({
     );
   }
   const page = Number(pageId ?? '1');
+  const handleCheckboxChange = () => {
+    dispatch(toggleItem(characterInfo));
+  };
 
   return (
-    <Link to={`/${page}/${characterInfo.id}`}>
-      <div
-        className="flex flex-col gap-2 bg-blue-500 rounded-3xl pt-4 px-2 sm:p-6 w-32 h-48 sm:w-48 sm:h-64 pb-0 sm:pb-0"
-        data-testid="character-card"
-        role="character-card"
-      >
-        <img
-          src={characterInfo?.image}
-          className="rounded-3xl w-24 h-24 mx-auto sm:w-32 sm:h-32"
-          alt="Character portrait"
-        />
-        <div className="text-xs text-white sm:font-bold sm:text-base overflow-hidden h-16 sm:h-full">
-          {characterInfo?.name}
+    <div
+      className="relative flex flex-col gap-2 bg-blue-500 rounded-3xl pt-4 px-2 sm:p-6 w-32 h-48 sm:w-48 sm:h-64 pb-0 sm:pb-0"
+      data-testid="character-card"
+      role="character-card"
+    >
+      <input
+        type="checkbox"
+        checked={!!selectedItems[characterInfo.id]}
+        onChange={handleCheckboxChange}
+        className="absolute top-0 right-0 sm:top-4 sm:right-4 w-4 h-4 hover:cursor-pointer border-blue-300 rounded-sm focus:ring-white focus:ring-2"
+      />
+      <Link to={`/${page}/${characterInfo.id}`}>
+        <div className="flex flex-col gap-2">
+          <img
+            src={characterInfo?.image}
+            className="rounded-3xl w-24 h-24 mx-auto sm:w-32 sm:h-32"
+            alt="Character portrait"
+          />
+          <div className="text-xs text-white sm:font-bold sm:text-base overflow-hidden h-16 sm:h-full">
+            {characterInfo?.name}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
