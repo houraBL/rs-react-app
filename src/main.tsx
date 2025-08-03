c4cecae0f3b817c5cd6a81b8b7328e5d03cc80ff
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-08-03 14:02:23 +0800

    feat: add state logic to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import HomePage from './pages/HomePage/HomePage.tsx';
 import AboutPage from './pages/AboutPage/AboutPage.tsx';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
 import DetailsPanel from './components/DetailsPanel/DetailsPanel.tsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
 
 const router = createBrowserRouter([
   {
@@ -49,7 +50,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ErrorBoundary>
   </StrictMode>
 );
